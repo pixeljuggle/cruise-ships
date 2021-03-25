e35ac7158ff1abd091748f7ee51a23eef237a977
Alex White <pixeljuggle@gmail.com>
2021-03-25 11:47:10 +0000

    Adding setSail method. Minor housekeeping

---
+++ index.js
@@ -0,0 +1,6 @@
+const Ship = require("./src/ships");
+
+const cruise = new Ship("Stockport", 10);
+
+console.log(cruise);
+console.log(cruise.atSea);
--- src/ships.js
+++ src/ships.js
@@ -1,11 +1,19 @@
 class Ship {
   constructor(startingPort = "unknown", passengerAboard = 0) {
-    this.startingPort = startingPort;
+    this.port = startingPort;
     this.passengersAboard = passengerAboard;
   }
 
   boardPassenger() {
     return this.passengersAboard++;
+  }
+
+  setSail() {
+    this.port = null;
+  }
+
+  get atSea (){
+    return this.port === null
   }
 }
 
